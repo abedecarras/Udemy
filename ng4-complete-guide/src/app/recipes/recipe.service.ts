06bcb041caedb9f96164979bb7f85f20fd9b5c93
abedecarras <abedecarras@gmail.com>
2020-05-11 20:28:15 -0300

    118.Using a service for cross-component communication

--- ng4-complete-guide/src/app/recipes/recipe.service.ts
+++ ng4-complete-guide/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
 
@@ -7,6 +8,8 @@ export class RecipeService {
     new Recipe('Another Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
+
+  recipeSelected = new EventEmitter<Recipe>();
 
   getRecipes() {
     return this.recipes.slice(); // .slice() returns a copy of recipes
