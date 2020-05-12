12b3015f5665d9fe674a3bd7def6e9b254d0633c
abedecarras <abedecarras@gmail.com>
2020-05-12 17:29:19 -0300

    155.Setting up routes

--- ng4-complete-guide/src/app/header/header.component.ts
+++ ng4-complete-guide/src/app/header/header.component.ts
@@ -6,17 +6,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() sectionSelected = new EventEmitter<string>();
-
   collapsed = true;
 
   constructor() { }
 
   ngOnInit() {
 
-  }
-
-  onSelect(section: string) {
-      this.sectionSelected.emit(section);
   }
  }
